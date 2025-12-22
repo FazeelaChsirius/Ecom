d5d1040182b9a5715b476ad92bcee19e46e75677
fazeelamushtaq <fazeela.mushtaq.sirius@gmail.com>
2025-12-23 00:08:25 +0500

    Create products slug

---
+++ app/products/[slug]/page.tsx
@@ -0,0 +1,14 @@
+import Slug from '@/components/Slug'
+import SlugInterface from '@/interface/slug.interface'
+import React, { FC } from 'react'
+
+const SlugRouter: FC<SlugInterface> = async ({ params }) => {
+  const slugRes = await fetch(`${process.env.SERVER}/api/product/${params.slug}`)
+  const data = slugRes.ok ? await slugRes.json() : null
+  
+  return (
+    <Slug data={data} title={params.slug}/>
+  )
+}
+
+export default SlugRouter
---
+++ components/Slug.tsx
@@ -0,0 +1,45 @@
+'use client'
+import DataInterface from '@/interface/data.interface'
+import priceCalculate from '@/lib/price-calculate'
+import { Button, Card, Empty } from 'antd'
+import Image from 'next/image'
+import React, { FC } from 'react'
+import '@ant-design/v5-patch-for-react-19';
+
+interface TitleInterface extends DataInterface{
+  title: string
+}
+
+const Slug: FC<TitleInterface> = ({ data, title }) => {
+  if(!data)
+    return <Empty />
+  console.log('slug-data', data)
+
+  return (
+    <div>
+      <Card className='shawod-lg'>
+        <div className='flex gap-12'>
+          <Image 
+            src={data.image}
+            width={280}
+            height={0}
+            alt={data.title}
+            className='rounded-lg object-cover'
+          />
+          <div>
+            <h1 className='text-5xl font-bold'>{data.title}</h1>
+            <p className='text-gray-500 mt-2'>{data.description}</p>
+            <div className='text-3xl font-medium flex gap-4 mt-5 mb-5'>
+              <h1>${priceCalculate(data.price, data.discount)}</h1>
+              <del className='text-slate-400'>{data.price}</del>
+              <h1 className='text-rose-500'>({data.discount}% Discount)</h1>
+            </div>
+              <Button type='primary' className='!bg-green-400 !font-medium !px-24 !py-6 !text-xl'>Buy now</Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+export default Slug
