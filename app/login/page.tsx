1399df4592fb156ddfa38c6144fd6209955e2192
fazeelamushtaq <fazeela.mushtaq.sirius@gmail.com>
2026-02-03 00:06:09 +0500

    Search Engine Optimization In Nextjs

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,25 @@
 import Login from '@/components/Login'
 import React from 'react'
 
+export const metadata = {
+  title: `Login - Ecom`,
+  description: 'Signin or login with your ecom account',
+  keywords: "Ecom signin, ecom login, ecom account login",
+  openGraph: {
+    title: `Login - Ecom`,
+    description: 'Signin or login with your ecom account',
+  url: `${process.env.SERVER}/login`,
+    siteName: "Ecom",
+    images: [
+      {
+        url: "/images/logo.jpg", // replace with your image
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+}
+
 const LoginRouter = () => {
   return (
     <Login />
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,25 @@
 import Signup from '@/components/Signup'
 import React from 'react'
 
+export const metadata = {
+  title: `Signup - Ecom`,
+  description: 'Signup or register your new ecom account',
+  keywords: "ecom signin, ecom login, ecom account login, ecom signup, ecom register, ecom new account",
+  openGraph: {
+    title: `Signup - Ecom`,
+    description: 'Signup or register your new ecom account',
+  url: `${process.env.SERVER}/signup`,
+    siteName: "Ecom",
+    images: [
+      {
+        url: "/images/logo.jpg", // replace with your image
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+}
+
 const SignupRouter = () => {
   return (
     <Signup />
